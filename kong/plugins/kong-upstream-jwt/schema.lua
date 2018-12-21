982b58271fbc366f49e9b2782ab52649e42dd7f4
Travis Raines <traines@konghq.com>
2018-12-21 12:26:09 -0800

    Enforce required fields in schema and add defaults

--- kong/plugins/kong-upstream-jwt/schema.lua
+++ kong/plugins/kong-upstream-jwt/schema.lua
@@ -3,12 +3,12 @@ local find = string.find
 
 return {
   fields = {
-    expiry = { type = "number"},
-    not_before = { type = "number"},
-    issuer = { type = "string"},
-    audience = { type = "string"},
-    subject = { type = "string"},
-    upstream_jwt_header = { type = "string"},
-    private_key_location = { type = "string"}
+    expiry = { type = "number", required = true, default = 900},
+    not_before = { type = "number", required = true, default = 0},
+    issuer = { type = "string", required = true},
+    audience = { type = "string", required = true},
+    subject = { type = "string", required = true},
+    upstream_jwt_header = { type = "string", required = true, default = "Authorization"},
+    private_key_location = { type = "string", required = true}
   }
 }
